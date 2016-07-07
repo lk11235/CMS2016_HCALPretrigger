1797169ac039ba0fc9ec7a09bbcbd740c86f9010
Lucas Kang <lucas_kang@brown.edu>
2016-07-07 17:08:06 +0200

    first commit

---
+++ CMSSW_8_0_11/src/crab_L1Tuple.py
@@ -0,0 +1,50 @@
+
+# example crab configuration file for one single run
+
+##________________________________________________________________________________||
+#Configurables
+
+dataset = '/JetHT/Run2016B-v2/RAW'
+run = '274088'
+
+##________________________________________________________________________________||
+
+jobname = dataset[1:].replace('/','__')
+jobname = jobname.replace(':','___')
+jobname = jobname.replace('RAW','RAW_'+run)
+
+##________________________________________________________________________________||
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+##________________________________________________________________________________||
+
+config.section_("General")
+config.General.requestName  = jobname
+config.General.workArea     = 'out_crab'
+
+##________________________________________________________________________________||
+
+config.section_("JobType")
+config.JobType.pluginName   = 'Analysis'
+config.JobType.psetName     = 'makeL1Ntuple_cfg.py'
+
+##________________________________________________________________________________||
+
+config.section_("Data")
+config.Data.inputDataset    = dataset
+config.Data.inputDBS        = 'global'
+config.Data.splitting       = 'LumiBased'
+config.Data.unitsPerJob     = 30 
+config.Data.publication     = True
+config.Data.publishDBS      = 'phys03'
+config.Data.runRange        = run
+
+##________________________________________________________________________________||
+
+config.section_("Site")
+config.Site.storageSite     = 'T2_CH_CERN'
+#config.Site.whitelist = ['T2_US_Caltech','T2_US_Florida', 'T2_US_MIT', 'T2_US_Nebraska', 'T2_US_Purdue', 'T2_US_UCSD', 'T2_US_Vanderbilt', 'T2_US_Wisconsin', 'T1_US_FNAL','T2_US_MIT']
+# you may want to uncomment this line and force jobs to run in the US
+# only a few datasets (mostly very new ones) will not be accessible
---
+++ CMSSW_8_0_11/src/makeL1Ntuple_cfg.py
@@ -0,0 +1,45 @@
+import FWCore.ParameterSet.Config as cms
+
+# make L1 ntuples from RAW+RECO
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("L1NTUPLE",eras.Run2_2016)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.Eras')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+
+# global tag
+process.GlobalTag.globaltag = '80X_dataRun2_HLT_v12'
+
+process.p = cms.Path(
+        process.RawToDigi
+        )
+
+process.schedule = cms.Schedule(process.p)
+
+from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleRAW,L1NtupleTFileOut 
+process = L1NtupleRAW(process)
+process = L1NtupleTFileOut(process) 
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+# Input source
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring() 
+process.source = cms.Source ("PoolSource",
+                             fileNames = readFiles,
+                             secondaryFileNames = secFiles
+                             )
+
+readFiles.extend( [
+    "/store/data/Run2016B/JetHT/RAW/v1/000/272/760/00000/7A42AE55-D513-E611-9D36-02163E011976.root",
+    ] )
